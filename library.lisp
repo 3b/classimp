ab482b73e017c6142e6e2dcca3bc31971b0f1fba
Bart Botta <00003b@gmail.com>
2013-02-22 02:47:53 -0600

    use correct syntax for multiple .so names to try

--- library.lisp
+++ library.lisp
@@ -3,6 +3,6 @@
 (define-foreign-library assimp
   (:windows "assimp.dll" );; :calling-convention :stdcall ?
   #++(:unix "libassimp.so")
-  (:unix "libassimp.so.3" "libassimp3.0.so" "libassimp.so"))
+  (:unix (:or "libassimp.so.3" "libassimp3.0.so" "libassimp.so")))
 
 (use-foreign-library assimp)
