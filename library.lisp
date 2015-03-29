0ea245faf33aaa1cb0b4a362f494a56ec97171b3
Bart Botta <00003b@gmail.com>
2015-03-28 23:04:27 -0500

    add alternate name for windows .dll

--- library.lisp
+++ library.lisp
@@ -2,6 +2,7 @@
 
 (define-foreign-library assimp
   (:darwin "libassimp.dylib")
+  (:windows (:or "assimp.dll" "libassimp.dll"));; :calling-convention :stdcall ?
   (:windows "assimp.dll" );; :calling-convention :stdcall ?
   #++(:unix "libassimp.so")
   (:unix (:or "libassimp.so.3" "libassimp3.0.so" "libassimp.so")))
